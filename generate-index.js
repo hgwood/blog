5ec4a135d7d1a649569383f0f3ca16bcadeef702
Hugo Wood <hx4.5@free.fr>
2015-12-11 01:03:00 +0100

    docs: added license info in readme

--- generate-index.js
+++ generate-index.js
@@ -9,7 +9,26 @@ const utils = require("./utils")
 
 const indexFilePath = path.join(__dirname, "README.md")
 
-fs.writeFileSync(indexFilePath,
+const renderIndex = postList => `
+# Hugo Wood's personal blog
+
+## Posts
+
+${postList}
+
+## License
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+  <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
+</a>
+<br />
+This work is licensed under a
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+  Creative Commons Attribution-ShareAlike 4.0 International License
+</a>.
+`
+
+fs.writeFileSync(indexFilePath, renderIndex(
   utils.posts()
     .map(post => `- ${post.yfm.date}: [${post.yfm.title}](${post.basename}/${post.basename}.md)`)
-    .join("\n"))
+    .join("\n")))
